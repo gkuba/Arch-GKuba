3a659f09a3bbbbf70f8b757caede8048adeed5a1
Geramie <gkuba@tuxgamer.com>
2026-04-09 21:10:11 -0600

    Initial commit adding README.md and install.sh

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Arch-GKuba
+# Debian-Gkuba
+
+This is my customization for fresh Debian installs.
+
+## Download Debian non-free netinstall
+
+Use the following Debian ISO as the base:
+
+- [Debian 11.5 Stable non-free][Debian ISO]
+
+[Debian ISO]: https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/11.5.0+nonfree/amd64/iso-cd/
+
+__NOTE:__ _do NOT grab the EDU download. This image includes non-free and firmware_
+
+### To Install
+This must be run as the `root` user.
+
+```bash
+bash <(wget -qO- https://raw.githubusercontent.com/gkuba/Debian-Gkuba/main/install.sh)
+```
+
+#### Install script information
+
+The `install.sh` script has been written in a modular way. This allows you to run parts of this or the whole thing.
+Full list of these functions below just append the one you want at the end of the command listed.
+
+This example will check for updates:
+
+```bash
+bash <(wget -qO- https://raw.githubusercontent.com/gkuba/Debian-Gkuba/main/install.sh) checkUpdates
+```
+
+##### Functions
+
+- Check for package updates
+
+```text
+checkUpdates
+```
+
+- Install Wanted Packages
+
+```text
+installPackages
+```
+
+This will install the packages listed under the `$WANTED_PACKAGES` variable
+
+###### Misc Info
+
+My bash prompt and settings can be found here [gkuba/dotfiles][gkuba/dotfiles]
+
+[gkuba/dotfiles]: https://github.com/gkuba/dotfiles
---
+++ install.sh
@@ -0,0 +1,96 @@
+#! /bin/bash
+
+function isPresent { command -v "$1" &> /dev/null && echo 1; }
+source /etc/os-release
+
+clear
+echo
+echo "Please wait, examining your system and network configuration..."
+echo
+
+ARCH=$(uname -m)
+PM_COMMAND=pacman
+PM_INSTALL="-S --noconfirm"
+PM_UPDATE="-Syu --noconfirm"
+# WGET_IS_PRESENT="$(isPresent wget)"
+WANTED_PACKAGES="git curl unzip nvim most ghostty code discord"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+ENDCOLOR="\e[0m"
+ITALICYELLOW="\e[3;33m"
+
+## Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo -e "${RED}You must be a root user to run this script, please run \"sudo su\" then try again.${ENDCOLOR}"
+  exit 1
+fi
+
+## Checks for wget
+# if [[ $WGET_IS_PRESENT -ne 1 ]]; then
+#   echo -e "${RED}Wget is not installed and required for this script to run. Please install wget then try again${ENDCOLOR}"
+#   exit 1
+# fi
+
+function checkUpdtes 
+{
+  echo
+  echo -e "${YELLOW}Checking for and installing updates...${ENDCOLOR}"
+  echo
+  $PM_COMMAND $PM_UPDATE
+}
+
+## Installs packages defined in WANTED_PACKAGES list.
+function installPackages
+{
+  echo
+  echo -e "${YELLOW}Installing packages ....${ENDCOLOR}"
+  echo -e "${YELLOW}Selected Packages: $WANTED_PACKAGES ${ENDCOLOR}"
+  echo
+  $PM_COMMAND $PM_INSTALL $WANTED_PACKAGES  
+}
+
+if [ -n "$*" ]; then
+  for ARG in $@
+  do
+    if ! type "$ARG" &> /dev/null; then
+      echo -e "${RED}No such function $ARG ${ENDCOLOR}"
+      exit 1
+    fi
+  done
+  
+  for ARG_RUN in $@
+  do
+    $ARG_RUN
+  done
+  echo
+  echo
+  echo -e "${GREEN}Done${ENDCOLOR}"
+  exit
+fi
+
+##############
+## ENTRY POINT
+##############
+
+echo
+echo
+echo -e "----------------------------"
+echo -e "    ${BLUE}Installation Summary${ENDCOLOR}    "
+echo -e "----------------------------"
+echo
+echo -e "Packages to Install:"
+echo -e $WANTED_PACKAGES
+echo 
+echo -e "${YELLOW}Ready to install, Press ENTER to continue or CTRL+C to cancel.${ENDCOLOR}"
+read -r 
+echo
+echo -e "${YELLOW}Installing please wait ...${ENDCOLOR}"
+
+installPackages
+
+echo -e "${GREEN}Installation complete.${ENDCOLOR}"
+
+exit 0
